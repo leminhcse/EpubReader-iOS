89f874fdf2b0eac36e444b67623429332f1cd02e
Minh Doan <doanleminhcse@gmail.com>
2022-09-26 00:34:20 +0700

    Create Side Menu View Controller

---
+++ EpubReader/Presentation/Main/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  EpubReader
+//
+//  Created by mac on 26/09/2022.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+    }
+
+}
---
+++ EpubReader/Presentation/Main/DisableAdsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DisableAdsViewController.swift
+//  EpubReader
+//
+//  Created by mac on 26/09/2022.
+//
+
+import UIKit
+
+class DisableAdsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+}
